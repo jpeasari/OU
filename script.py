72cf16bc2ba38ed7f84a2d19619732b4f68b21d7
John Reddy Peasari <55759107+jpeasari@users.noreply.github.com>
2022-05-10 14:28:18 -0500

    Update script.py

--- script.py
+++ script.py
@@ -1,1 +1,25 @@
+import os
+name1 = 'barcodes.tsv.gz'
+name2 = 'matrix.mtx.gz'
+name3 = 'features.tsv.gz'
+os.chdir('D:/Dave/snRNA/all_data/')
 
+for files in os.listdir():
+    print(files)
+    
+ for samples in os.listdir():
+    os.chdir(samples)
+    for file in os.listdir():
+        split_tup = os.path.splitext(file)
+        file_ext = split_tup[1]
+        if file.endswith('barcodes.tsv.gz'):
+            os.rename(file, name1)
+            
+        if file.endswith('features.tsv.gz'):
+            os.rename(file,name3)
+            
+        if file.endswith('matrix.mtx.gz'):
+                os.rename(file,name2)
+
+    os.chdir('../')
+      
